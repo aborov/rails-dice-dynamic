d438943367486cf7d490dc2a3b53498c53f103c8
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-06-07 01:43:54 +0000

    finished random one

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -20,7 +20,7 @@ class DiceController < ApplicationController
 
     2.times do
       die = rand(1..10)
-  
+
       @rolls.push(die)
     end
     render({ :template => "two_ten" })
@@ -31,11 +31,10 @@ class DiceController < ApplicationController
 
     1.times do
       die = rand(1..20)
-  
+
       @rolls.push(die)
     end
     render({ :template => "one_twenty" })
-
   end
 
   def five_four
@@ -43,11 +42,30 @@ class DiceController < ApplicationController
 
     5.times do
       die = rand(1..4)
-  
+
       @rolls.push(die)
     end
     render({ :template => "five_four" })
   end
 
+  def random
+    dice_count = params[:sides].to_i
+    sides_count = params[:dice].to_i
 
+    if dice_count <= 0 || sides_count <= 0
+      render ({ :template => "error" })
+      return
+    end
+
+    @dice_result = roll_dice(sides_count, dice_count)
+    render ({ :template => "roll" })
+  end
+
+  def roll_dice(dice_count, sides_count)
+    dice_rolls = []
+    dice_count.times do
+      dice_rolls << rand(1..sides_count)
+    end
+    return { :dice => dice_count, :sides => sides_count, :rolls => dice_rolls }
+  end
 end
